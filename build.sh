c5ce9643951f83488d031f789edb7f111a3a4e1f
AnthonyLoche <anthonylocheifc@gmail.com>
2024-05-10 20:53:58 -0300

    ops

--- build.sh
+++ build.sh
@@ -8,8 +8,5 @@ pip install --upgrade pip
 # Instala as dependências
 pip install -r requirements.txt
 
-# Coleta os arquivos estáticos
-python manage.py collectstatic --no-input
-
 # Aplica as migrações
 python manage.py migrate
